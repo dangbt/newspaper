0c947887af3f1a64d7cfe6038a7161264eea6a5b
TAN DANG <dangbt@TANs-MacBook-Pro.local>
2020-10-14 10:36:16 +0700

    fix build without css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,4 @@
-import "../styles/blueprint.css";
 import "../styles/globals.css";
-import "@blueprintjs/core";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true
-  },
-  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  future: {},
+  purge: [],
   target: "relaxed",
   prefix: "",
   important: false,
